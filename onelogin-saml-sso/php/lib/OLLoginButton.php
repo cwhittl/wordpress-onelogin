5a4f0a67a030fb896a48d73af896acbec21dbfc2
Whittle <chriswhittle@collectivebias.com>
2016-01-26 09:38:48 -0600

    added filters for site url and redirect

--- onelogin-saml-sso/php/lib/OLLoginButton.php
+++ onelogin-saml-sso/php/lib/OLLoginButton.php
@@ -4,34 +4,44 @@ in addition to the wordpress login.
 */
 class OLLoginButton
 {
-    protected $redirect_to = "";
-    function __construct() {
-        add_action('init', array($this, "login_form"));
-        add_action('init', array($this, "redirect_to_ol"));
+    protected $redirect_to = '';
+    public function __construct()
+    {
+        add_action('init', array($this, 'login_form'));
+        add_action('init', array($this, 'redirect_to_ol'));
 
         add_filter('login_redirect', array($this, 'get_login_redirect'), 100, 3);
     }
-    function login_form() {
+    public function login_form()
+    {
         add_action('login_form', array($this, 'add_onelongin_button'));
     }
-    function get_login_redirect($redirect_to, $requested_redirect_to, $user) {
+    public function get_login_redirect($redirect_to, $requested_redirect_to, $user)
+    {
         if ($user->ID == 0) {
             $this->redirect_to = $redirect_to;
-        }else{
+        } else {
             wp_redirect($redirect_to);
-             die();
+            die();
         }
+
         return $redirect_to;
     }
-    function add_onelongin_button() {
+    public function add_onelongin_button()
+    {
+        /*
+        Filters are used so that you can change the siteurl and redirect.
+        This is helpful if you have a subdomain multisite that is using domain mapping
+        but you want to use a single onelogin app for all. */
         if (function_exists('initialize_saml')) {
             $site_url = wp_login_url();
-            if ($this->redirect_to != "") {
+            $site_url = apply_filters('ol_loginbutton_siteurl', $site_url);
+            if ($this->redirect_to != '') {
                 $redirect = $this->redirect_to;
-                $url = $site_url . "?ForceOL=" . $redirect;
-            }
-            else {
-                $url = $site_url . "?ForceOL=true";
+                $redirect = apply_filters('ol_loginbutton_redirect', $redirect);
+                $url = $site_url.'?ForceOL='.$redirect;
+            } else {
+                $url = $site_url.'?ForceOL=true';
             }
             $image_url = plugins_url('OneLoginButton.png', __FILE__);
             /*
@@ -57,20 +67,19 @@ EOT;
         }
     }
 
-    function redirect_to_ol() {
-        if (function_exists('initialize_saml') && isset($_GET["ForceOL"]) && !is_user_logged_in()) {
-            if (!filter_var($_GET["ForceOL"], FILTER_VALIDATE_URL) === false) {
+    public function redirect_to_ol()
+    {
+        if (function_exists('initialize_saml') && isset($_GET['ForceOL']) && !is_user_logged_in()) {
+            if (!filter_var($_GET['ForceOL'], FILTER_VALIDATE_URL) === false) {
                 //Valid URL
-                $url = $_GET["ForceOL"];
-            }
-            else {
+                $url = $_GET['ForceOL'];
+            } else {
                 $url = $_SERVER['REQUEST_URI'];
             }
             $auth = initialize_saml();
             if (!empty($url)) {
                 $auth->login($url);
-            }
-            else {
+            } else {
                 $auth->login();
             }
         }
